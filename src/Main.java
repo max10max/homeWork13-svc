9072de73c21b33f8a25da979dbe37c894b0b98d1
Maksim Suvorov <suvormaks@yandex.ru>
2022-10-26 21:55:36 +0300

    added branch longer. switch int -> long in SalesManager.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {1, 2, 5, 3};
+        long[] sales = {1, 2, 5, 3};
         SalesManager manager1 = new SalesManager(sales);
         System.out.println("Максимальная сумма продажи " + manager1.max());
 
