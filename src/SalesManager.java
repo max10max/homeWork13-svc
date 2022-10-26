818b8f2b0fc78dc240b22df4ce43b30a45a7f2d0
Maksim Suvorov <suvormaks@yandex.ru>
2022-10-26 22:29:07 +0300

    added metod croppedAverageSales

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,5 +1,6 @@
 public class SalesManager {
     protected int[] sales;
+
     public SalesManager(int[] sales) {
         this.sales = sales;
     }
@@ -12,5 +13,21 @@ public class SalesManager {
             }
         }
         return max;
+    }
+
+    public int croppedAverageSales() {
+        int max = 0;
+        int min = sales[0];
+        int sum = 0;
+        for (int sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+            if (sale < min) {
+                min = sale;
+            }
+            sum += sale;
+        }
+        return sum-max-min;
     }
 }
